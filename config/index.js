1f19d0ee382b1c597668a763242463d37ba294c6
jack <45559783+muabl@users.noreply.github.com>
2022-09-23 01:11:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oXblv57HlQDa2A4SF1F2EbdmTmoA",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "f7vqUKygvd4SBQ0t0p7dGvckjdBMzUREw4TcoJf4rdw",
+      useTemplateId: "	Ghxz5MNfwZIeFwWVcEGOeIWgv4wkaNx7y4IpMGSCtrA",
       // 所在省份
       province: "福建",
       // 所在城市
@@ -98,7 +98,7 @@ export const USER_CONFIG = {
     {
       name: "老婆1",
       id: "oXblv51AKav2htVzLugNAymawy2s",
-      useTemplateId: "f7vqUKygvd4SBQ0t0p7dGvckjdBMzUREw4TcoJf4rdw",
+      useTemplateId: "	Ghxz5MNfwZIeFwWVcEGOeIWgv4wkaNx7y4IpMGSCtrA",
       province: "福建",
       city: "福州",
       horoscopeDate: '06-28',
