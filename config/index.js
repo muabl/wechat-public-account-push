906f3eac8955c2ea2cfaa05f20720856beb861ee
jack <45559783+muabl@users.noreply.github.com>
2022-09-22 23:47:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -148,7 +148,7 @@ export const USER_CONFIG = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oXblv57HlQDa2A4SF1F2EbdmTmoA",
+      id: "",
     },
     // 你可以不断按格式往下增加
     // ...
